a867c159dacdb9b3e122b05c7177ede8a6e1dccb
Goultarde <Harounacoulibaly74@yahoo.com>
2023-01-12 00:29:36 +0100

    commit

--- admin/delete_post.php
+++ admin/delete_post.php
@@ -1,11 +1,13 @@
 <?php
 // Inclure le fichier header.php
+include 'includes/header.php';
 // Inclure le fichier sidebar.php
+include 'includes/sidebar.php';
 ?>
 
 <?php
 // Inclure la variable $db = new Database();
-
+$db = new Database();
 // Si la méthode de requête est GET
 // Alors
 //     Récupérer la valeur de del_postid
@@ -22,8 +24,34 @@
 //         Si les données sont supprimées
 //             Alors
 //                 Afficher un message de succès
-//                 Rediriger vers post_list.php
-//             Sinon
-//                 Afficher un message d'erreur
-//                 Rediriger vers post_list.php
+if ($_SERVER['REQUEST_METHOD'] == 'GET') {
+    $del_postid = $_GET["del_postid"] ;
+}
+if (!isset($del_postid) || $del_postid == NULL ){
+    header('location:category_list.php');
+    }else{
+        $del_postid = $_GET["del_postid"] ;
+        
+    }
+?>
+
+
+<?php
+ob_start();
+// Inclure le fichier header.php
+include "includes/header.php";
+// Inclure le fichier sidebar.php
+include "includes/sidebar.php";
+?>
+<?php
+$id = $_GET['catid'];
+$query = "DELETE FROM category WHERE category_id=$id";
+$delete_cat =  $db->delete($query);
+
+?>
+<?php
+// Inclure le fichier footer.php
+include 'includes/footer.php';
+header('location:category_list.php');
+ob_end_flush();
 ?>
