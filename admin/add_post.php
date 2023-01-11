ce6e14b04f813b775ff1ff6eedb95481eab39ff7
Goultarde <Harounacoulibaly74@yahoo.com>
2023-01-11 14:49:20 +0100

    first commit

---
+++ admin/add_post.php
@@ -0,0 +1,111 @@
+﻿<?php
+// Inclure le fichier header.php
+// Inclure le fichier sidebar.php
+?>
+<div class="grid_10">
+
+    <div class="box round first grid">
+        <h2>Ajouter un nouveau post</h2>
+        <?php
+        // Si la méthode de requête est POST
+        if ($_SERVER['REQUEST_METHOD'] == 'POST'){
+        
+        $category_id = $_POST ['category_id'];   
+        $title = $_POST ['title'];
+        $body = $_POST ['body']; 
+        $image = $_POST ['image'];
+        $author = $_POST ['author'];
+        $tags = $_POST ['tags'];
+
+        
+        if(empty($title)) {
+            echo 'Title est un champs oblligatoire';
+        }else{
+            $add_post = "INSERT INTO post(category_id,title,body,image,author,tags) where values='?,?,?,?,?,?' ";
+            $post = $db->crate($add_post);
+        }
+        //  Si le post est inséré
+        if ($add_post) { 
+            echo "success";
+        }
+        else{
+            echo "Error";
+        }}
+        ?>
+        <div class="block">
+            <form action="" method="post" enctype="multipart/form-data">
+                <table class="form">
+
+                    <tr>
+                        <td>
+                            <label>Title</label>
+                        </td>
+                        <td>
+                            <input type="text" name="title" placeholder="Entrez le titre du post" class="medium" />
+                        </td>
+                    </tr>
+
+                    <tr>
+                        <td>
+                            <label>Category</label>
+                        </td>
+                        <td>
+                            <select id="select" name="category_id">
+                                <option>Select Category </option>
+                                <?php
+                                // Récupérer les catégories de la table category
+                                // Tant que les catégories sont récupérées
+                                //     Afficher les catégories dans la liste déroulante
+                                ?>
+                                <option value=""></option>
+                            </select>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <label>Télecharger une image</label>
+                        </td>
+                        <td>
+                            <input type="file" name="image" />
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <label>Nom de l'auteur</label>
+                        </td>
+                        <td>
+                            <input type="text" name="author" placeholder="Entrez le nom de l'auteur." />
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <label>Tags</label>
+                        </td>
+                        <td>
+                            <input type="text" name="tags" placeholder="Entrez le tag ici ..." />
+                        </td>
+                    </tr>
+                    <tr>
+                        <td style="vertical-align: top; padding-top: 9px;">
+                            <label>Content</label>
+                        </td>
+                        <td>
+                            <textarea class="tinymce" name="body"></textarea>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td></td>
+                        <td>
+                            <input type="submit" name="submit" Value="Sauvegarder" />
+                        </td>
+                    </tr>
+                </table>
+            </form>
+        </div>
+    </div>
+</div>
+<!-- Load TinyMCE -->
+<script src="js/tiny-mce/jquery.tinymce.js" type="text/javascript"></script>
+<?php
+// Inclure le fichier footer.php
+?>
